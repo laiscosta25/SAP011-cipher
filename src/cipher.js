2f226c1e0c1db2e8b29286a9e4d395a069e43a41
laiscosta25 <laiscosta.c2@gmail.com>
2023-08-04 17:01:29 -0300

    escrever uma mensagem aqui

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,47 @@
 const cipher = {
+
+  encode: function (offset, string) {
+     if (!offset) {
+       throw new TypeError();
+     }
+
+console.log(offset, string)
+    let textCode = "";
+
+    for (let i = 0; i < string.length; i++) {
+      let cifrar = string.charCodeAt(i);
+
+      if (cifrar >= 65 && cifrar <= 90) {
+        textCode += String.fromCharCode(((cifrar - 65 + offset) % 26) + 65);
+      } else if (cifrar >= 97 && cifrar <= 122) {
+        textCode += String.fromCharCode(((cifrar - 97 + offset) % 26) + 97);
+      } else {
+        textCode += string.charAt(i);
+      }
+    }
+    return textCode;
+  },
+
+  decode: function (offset, string) {
+    if (!offset) {
+      throw new TypeError();
+    }
+    let textDecode = "";
+
+    for (let i = 0; i < string.length; i++) {
+      let decifrar = string.charCodeAt(i);
+
+      if (decifrar >= 65 && decifrar <= 90) {
+        textDecode += String.fromCharCode(((decifrar + 65 - offset) % 26) + 65);
+      } else if (decifrar >= 97 && decifrar <= 122) {
+        textDecode += String.fromCharCode(((decifrar - 122 - offset) % 26) + 122);
+      } else {
+        textDecode += string.charAt(i);
+      }
+    }
+    return textDecode;
+  },
   // ...
 };
 
-export default cipher;
+export default cipher
--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//window.alert("Bem vindes ao lugar que te ajudará a se comunicar de um jeito secreto!")
+
+const buttonCipher = document.getElementById("buttonCipher");
+const buttonDecipher = document.getElementById("buttonDecipher");
+
+document.getElementById("buttonCipher").addEventListener("click", cipherTextReturn);
+document.getElementById("buttonDecipher").addEventListener("click", decipherTextReturn);
+
+function cipherTextReturn(e) {
+    e.preventDefault()
+    const offset = parseInt(document.getElementById("offsetNumber1").value)
+    const string = document.getElementById("ToCodeText").value
+    const resultCipher = cipher.encode(offset, string);
+    const showResult = document.getElementById("ResultCodeText")
+    showResult.value = resultCipher;
+    // console.log(document.getElementById("offsetNumber1").value)
+}
+
+function decipherTextReturn(e) {
+    e.preventDefault()
+    const offset = parseInt(document.getElementById("offsetNumber1").value)
+    const string = document.getElementById("ToCodeText").value
+    const resultDecipher = cipher.decode(offset, string)
+    const showResult = document.getElementById("ResultCodeText")
+    showResult.value = resultDecipher;
+}
